c8506da2856ac595705d7e29e6bf9e58057d0843
Ancy <ancy.cs24@stellamaryscoe.edu.in>
2026-07-05 12:47:53 +0530

    frist commit

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@edutrack.com', 'Ancy@2026')" | python manage.py shell
